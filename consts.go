406fc89eddabc505fa7d3dc8ddc1464dce0c40b5
forsigg <al.berhmiller@gmail.com>
2025-01-05 01:33:53 +0700

    draw sprite sheet

---
+++ consts.go
@@ -0,0 +1,7 @@
+package main
+
+// PATH AND DIRS
+const (
+	ASSETS_PATH string = "./assets/"
+	IMAGES_PATH string = ASSETS_PATH + "images/"
+)
--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	PlayerImage *ebiten.Image
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.RGBA{120, 180, 255, 255})
+
+	// draw player image
+	screen.DrawImage(
+		g.PlayerImage,
+		&ebiten.DrawImageOptions{},
+	)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -23,9 +32,17 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Awesome Game")
+	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	playerImg, _, err := ebitenutil.NewImageFromFile(IMAGES_PATH + "ninja_sprite.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := ebiten.RunGame(&Game{
+		PlayerImage: playerImg,
+	}); err != nil {
 		log.Fatal(err)
 	}
 }
